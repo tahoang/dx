b2c5dc7bd28f02d2d8207600fb7571e6cb087c87
tahoang <dhoang519@gmail.com>
2014-03-06 11:42:42 -0500

    added layer options for leaflet viewer

--- src/map/leafletViewer.js
+++ src/map/leafletViewer.js
@@ -12,7 +12,7 @@ dx.map.LeafletViewer = dx.define({
     name: 'LeafletViewer',
     extend: dx.map.MapViewer,
     _className: 'LeafletViewer',
-    initialize: function (options) {
+    initialize: function(options) {
         dx.map.MapViewer.prototype.initialize.apply(this, arguments);
         //cloudmade layer
         var cloudMadeUrl = 'http://{s}.tile.cloudmade.com/c0925b1494384159af7cd710aadbda8d/997/256/{z}/{x}/{y}.png';
@@ -35,27 +35,57 @@ dx.map.LeafletViewer = dx.define({
             //new L.Marker([38.9, -76.0]).bindPopup('Eastern shore company'),
             //new L.Marker([38.36, -75.59]).bindPopup('Salisbury University')
         ]);
+        this.geoJsonFeatures = L.geoJson();
         this.map = L.map('map', {
-            layers: [osm, this.features],
-            center: new L.LatLng(39.0, -76.70),
-            zoom: 7,
+            layers: [osm, this.features, this.geoJsonFeatures],
+            center: this.center || new L.LatLng(39.0, -76.70),
+            zoom: this.zoomLevel || 7,
             measureControl: true
         });
         //set up layer control
-        //var baseMaps = {
-        //    'CloudMade': cmLayer,
-        //    'OpenStreetMap': osm
-        //};
-
-        //var overlayMaps = {
-        //    //other overlay layers go here
-        //    //feature layer
-        //    'Features': this.features
-        //};
-        //L.control.layers(baseMaps, overlayMaps).addTo(this.map);
+        var baseMaps = {
+            'CloudMade': cmLayer,
+            'OpenStreetMap': osm
+        };
+        if (typeof this.baseLayers != 'undefined')
+            for (var i in this.baseLayers) {
+                var layer = this.baseLayers[i];
+                if (layer !== undefined)
+                    baseMaps[i] = layer;
+            }
+        var overlayMaps = {
+            //other overlay layers go here
+            //feature layer
+            //'Features': this.features,
+            'Overlays': this.geoJsonFeatures
+        };
+        if (typeof this.overlays != 'undefined') {
+            for (var i in this.overlays) {
+                var layer = this.overlays[i];
+                overlayMaps[i] = layer;
+            }
+        }
+        L.control.layers(baseMaps, overlayMaps).addTo(this.map);
         L.control.scale().addTo(this.map);
     },
-    addFeatureToFeatureGroup: function (feature) {
+    addGeoJsonLayer: function(data, option) {
+        if (typeof data == 'undefined') {
+            dx.log('No data found')
+            return;
+        }
+        dx.log('Adding data to map...');
+        if (this.geoJsonFeatures != 'undefined') {
+            if (typeof option == 'undefined')
+                this.geoJsonFeatures.addLayer(L.geoJson(data));
+            else
+                this.geoJsonFeatures.addLayer(L.geoJson(data, option));
+        }
+    },
+    clearGeoJsonFeatures: function() {
+        if (this.geoJsonFeatures != 'undefined')
+            this.geoJsonFeatures.clearLayers();
+    },
+    addFeatureToFeatureGroup: function(feature) {
         var features = this.features;
         if (typeof features == 'undefined') {
             dx.log('No feature group found');
@@ -64,7 +94,7 @@ dx.map.LeafletViewer = dx.define({
         if (feature != null)
             features.addLayer(feature);
     },
-    clearFeatures: function () {
+    clearFeatures: function() {
         var features = this.features;
         if (typeof features == 'undefined') {
             dx.log('No feature group found');
@@ -72,13 +102,13 @@ dx.map.LeafletViewer = dx.define({
         }
         features.clearLayers();
     },
-    createFeature: function (obj) {
+    createFeature: function(obj) {
         var wkt = new Wkt.Wkt();
         wkt.read(obj);
         var f = wkt.toObject();
         return f;
     },
-    getFeaturesBound: function () {
+    getFeaturesBound: function() {
         var features = this.features;
         if (typeof features == 'undefined') {
             dx.log('No feature group found');
@@ -86,12 +116,26 @@ dx.map.LeafletViewer = dx.define({
         }
         return features.getBounds();
     },
-    zoomToFeatures: function () {
-        var bounds = this.getFeaturesBound();
-        this.map.fitBounds(bounds);
+    getGeoJsonFeaturesBound: function() {
+        var features = this.geoJsonFeatures;
+        if (typeof features == 'undefined') {
+            dx.log('No geojson feature found');
+            return;
+        }
+        return features.getBounds();
     },
-    zoomToPoint: function (point, zoom) {
-        var z = zoom || 10;//default zoom
+    zoomToFeatures: function() {
+        var bounds = this.getFeaturesBound();
+        if (typeof bounds != 'undefined')
+            this.map.fitBounds(bounds);
+    },
+    zoomToGeoJsonFeatures: function() {
+        var bounds = this.getGeoJsonFeaturesBound();
+        if (typeof bounds != 'undefined')
+            this.map.fitBounds(bounds);
+    },
+    zoomToPoint: function(point, zoom) {
+        var z = zoom || this.getMaxZoom();//default zoom
         if (typeof point.x != 'undefined' && typeof point.y != 'undefined') {
             var latlng = new L.LatLng(point.x, point.y);
             this.map.setView(latlng, z);
